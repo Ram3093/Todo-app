5ab297bf7ad26ef08d108579075dff7ee5e79496
Ram3093 <65605721+Ram3093@users.noreply.github.com>
2020-07-10 13:42:21 +0530

    Updates

---
+++ precache-manifest.ef6bbc0bce13e58e202e0d0250fb954b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8920aacd5387015acdde0c340f0f84c",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "425558843445074872d0",
+    "url": "/Todo-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "c4236174b61a25951732",
+    "url": "/Todo-app/static/js/2.0b1e0712.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.0b1e0712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425558843445074872d0",
+    "url": "/Todo-app/static/js/main.cef357aa.chunk.js"
+  },
+  {
+    "revision": "5f1c12c73f7411ae99ee",
+    "url": "/Todo-app/static/js/runtime-main.f8198d83.js"
+  }
+]);
